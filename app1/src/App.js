f6b24d2573bb769ac0ace82602f0ef77f07dbaf3
pongkitkkpk <pongkitkim@gmail.com>
2023-08-18 15:27:38 +0700

    event Data

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Header,Content,Footer } from './func-components'
-import Calendar from './class-components'
-import Banner from './banner'
-import { Calculator2 } from './calculator'
+// import { Header,Content,Footer } from './func-components'
+// import Calendar from './class-components'
+// import Banner from './banner'
+// import { Calculator2 } from './calculator'
+import { EventData2 } from './event-data'
 
 function App() {
   return (
     <>
-      <Header/>
+      {/* <Header/>
       <p><center><Calendar/></center></p>
       <Content/>
       <Banner/>
       <Calculator2/>
-      <Footer/>
+      <Footer/> */}
+      <EventData2/>
       
     </>
   )
