8287421968248a9c0339c0dfec774279cfa6cd40
pongkitkkpk <pongkitkim@gmail.com>
2023-08-18 13:55:43 +0700

    old word form 1 week

---
+++ app1/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header,Content,Footer } from "./func-components";
+import Calendar from "./class-components";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <p><center><Calendar/></center></p>
+      <Content/>
+      <Footer/>
+    </>
+  )
+}
+export default App
